f708a9a5a9156d37e8daf9d669266a8ac2e9e151
Kasey <KaseyColian@gmail.com>
2018-01-26 11:33:18 -0500

    Added Instance Variables, Constructors, Accessors of Methods, and Override string.  Need to make tick method and create methods for interaction.

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -2,4 +2,53 @@ package virtualpet;
 
 public class VirtualPet {
 
+	// VirtualPet bobTheDragon = new VirtualPet();
+	// instance variables
+	
+	public String getStatus;
+	public int hungerLevel;
+	public int thirstLevel;
+	public int wasteLevel;
+	public int boredomLevel;
+	public int sicknessLevel;
+
+	// Constructor
+
+	public VirtualPet(int hungerLevel, int thirstLevel, int wasteLevel, int boredomLevel, int sicknessLevel) {
+		this.hungerLevel = hungerLevel;
+		this.thirstLevel = thirstLevel;
+		this.wasteLevel = wasteLevel;
+		this.boredomLevel = boredomLevel;
+		this.sicknessLevel = sicknessLevel;}
+		
+		//Accessor of Method
+	public String getStatus() {
+		return getStatus;
+	}
+	
+	public int getHungerLevel() {
+		return hungerLevel;
+	}
+
+	public int getThirstLevel() {
+		return thirstLevel;
+	}
+
+	public int getWasteLevel(){
+		return wasteLevel;
+	}
+
+	public int boredomLevel(){
+		return boredomLevel;
 }
+	public int sicknessLevel() {
+		return sicknessLevel;
+		
+	}
+	public String toStatus() {
+		return "Hunger is: " + hungerLevel + "\nThirst is: " + thirstLevel + "\nWaste is: " + wasteLevel + "\nBoredom is: " + boredomLevel + "\nSickness is: " + sicknessLevel;
+	}
+	}
+
+}
+
